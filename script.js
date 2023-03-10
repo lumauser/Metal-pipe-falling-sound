7e16dc4d80cf15216793cc2bf179ed5dbe033a72
lumauser <91039519+lumauser@users.noreply.github.com>
2023-03-10 12:42:01 -0800

    test purpose

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById("myButton");
+const image = document.getElementById("myImage");
+
+button.addEventListener("click", () => {
+  image.src = "Resources/MetalPipeMain.jpeg";
+});
